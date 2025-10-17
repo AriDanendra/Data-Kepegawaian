3384fed6973a4dd50ef0b1bc6dee00471349a7a6
AriDanendra <135611273+AriDanendra@users.noreply.github.com>
2025-10-17 08:57:33 +0800

    vercel config

--- Datapegawaibackend/index.js
+++ Datapegawaibackend/index.js
@@ -7,12 +7,10 @@ import { fileURLToPath } from 'url';
 import fs from 'fs';
 
 const app = express();
-const PORT = 3001;
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const uploadDir = path.join(__dirname, 'public/uploads');
+const uploadDir = path.join(__dirname, '../public/uploads'); // Sesuaikan path untuk Vercel
 if (!fs.existsSync(uploadDir)){
     fs.mkdirSync(uploadDir, { recursive: true });
 }
@@ -20,11 +18,11 @@ if (!fs.existsSync(uploadDir)){
 app.use(cors());
 app.use(express.json());
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
+// Gunakan prefix /api untuk semua rute
 app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+// Export app untuk Vercel
+export default app;
