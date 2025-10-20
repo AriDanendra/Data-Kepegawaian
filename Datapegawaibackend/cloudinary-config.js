8419b81357498f6b196537c7943719456964dcdc
AriDanendra <135611273+AriDanendra@users.noreply.github.com>
2025-10-20 15:21:04 +0800

    fix back

--- Datapegawaibackend/cloudinary-config.js
+++ Datapegawaibackend/cloudinary-config.js
@@ -13,20 +13,16 @@ cloudinary.config({
 
 const storageOptions = {
   cloudinary: cloudinary,
-  params: async (req, file) => {
-    // Tentukan flag berdasarkan tipe file
-    const flags = file.mimetype === 'application/pdf' ? ['attachment'] : [];
-
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const originalName = file.originalname.split('.').slice(0, -1).join('.');
-
-    return {
-      folder: 'data-kepegawaian',
-      allowed_formats: ['jpg', 'jpeg', 'png', 'pdf'],
-      resource_type: 'auto',
-      public_id: `${originalName}-${uniqueSuffix}`,
-      flags: flags, // Terapkan flag di sini
-    };
+  params: {
+    folder: 'data-kepegawaian',
+    allowed_formats: ['jpg', 'jpeg', 'png', 'pdf'],
+    // Tambahkan baris ini
+    resource_type: 'auto', 
+    public_id: (req, file) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      const originalName = file.originalname.split('.').slice(0, -1).join('.');
+      return `${originalName}-${uniqueSuffix}`;
+    },
   },
 };
 const storage = new CloudinaryStorage(storageOptions);
